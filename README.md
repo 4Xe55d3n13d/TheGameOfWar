2ac46cdc75d514ce0d34b3e92409a84c832e2359
4Xe55d3n13d <allaccessdenied@protonmail.com>
2022-12-21 11:46:40 -0500

    now with cleaner code

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 A simple, automated War simulator based on one of my favorite childhood games made using Python 3.
 # 
 # How to Play:
-Say yes to commence a battle. Say no to kill the program.
+Say yes to commence a battle. Say no to kill the program. The deck of cards is infinite.
 # 
 # How to run The Game of War:
 # !!MAKE SURE YOU HAVE PYTHON 3.10 OR LATER INSTALLED!!
--- user.py
+++ user.py
@@ -1,81 +1,20 @@
 import sys
 import random
+from random import choice
 
 print("================")
 print("The Game of War.")
 print("by allaccessdenied.")
 print("================")
 commence = str(input("Commence Battle? Yes or No.").lower())
+
 def cardPicker(comp,play):
-    cards = ["",""]
-
-    twocards = ["2H","2D","2S","2C"]
-    threecards = ["3H","3D","3S","3C"]
-    fourcards = ["4H","4D","4S","4C"]
-    fivecards = ["5H","5D","5S","5C"]
-    sixcards = ["6H","6D","6S","6C"]
-    sevencards = ["7H","7D","7S","7C"]
-    eightcards = ["8H","8D","8S","8C"]
-    ninecards = ["9H","9D","9S","9C"]
-    tenroyalty = ["JH","JD","JS","JC"]
-    elevenroyalty = ["QH","QD","QS","QC"]
-    twelveroyalty = ["KH","KD","KS","KC"]
-    thirteenroyalty = ["AH","AD","AS","AC"] 
-
-    match comp:
-        case 2:
-            cards[0] = random.choice(twocards)
-        case 3:
-            cards[0] = random.choice(threecards)
-        case 4:
-            cards[0] = random.choice(fourcards)
-        case 5:
-            cards[0] = random.choice(fivecards)
-        case 6:
-            cards[0] = random.choice(sixcards)
-        case 7:
-            cards[0] = random.choice(sevencards)
-        case 8:
-            cards[0] = random.choice(eightcards) 
-        case 9:
-            cards[0] = random.choice(ninecards)
-        case 10:
-            cards[0] = random.choice(tenroyalty)
-        case 11:
-            cards[0] = random.choice(elevenroyalty)
-        case 12:
-            cards[0] = random.choice(twelveroyalty)
-        case 13:
-            cards[0] = random.choice(thirteenroyalty)
-    match play:
-        case 2:
-            cards[1] = random.choice(twocards)
-        case 3:
-            cards[1] = random.choice(threecards)
-        case 4:
-            cards[1] = random.choice(fourcards)
-        case 5:
-            cards[1] = random.choice(fivecards)
-        case 6:
-            cards[1] = random.choice(sixcards)
-        case 7:
-            cards[1] = random.choice(sevencards)
-        case 8:
-            cards[1] = random.choice(eightcards) 
-        case 9:
-            cards[1] = random.choice(ninecards)
-        case 10:
-            cards[1] = random.choice(tenroyalty)
-        case 11:
-            cards[1] = random.choice(elevenroyalty)
-        case 12:
-            cards[1] = random.choice(twelveroyalty)
-        case 13:
-            cards[1] = random.choice(thirteenroyalty)
-    return cards
+    vals = list(range(2, 11)) + list('JQKA')
+    deck = [[str(val) + suit for suit in 'HDSC'] for val in vals]
+    return [choice(deck[comp]),choice(deck[play])]
 
 while(commence == "yes"):
-    values = [2,3,4,5,6,7,8,9,10,11,12,13]
+    values = [2,3,4,5,6,7,8,9,10,11]
     
     cpu = random.choice(values)
     player = random.choice(values)
